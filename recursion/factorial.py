1eb72ef507d930a889a8168015bdf1087a2ed63f
Iago Breno <ibacaraujo@gmail.com>
2018-10-11 04:56:01 +0000

    Update factorial.py file

--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,5 +1,15 @@
+#Define recursive factorial function
+
 def factorial(n):
   if n == 0:
     return 1
   else:
     return n * factorial(n-1)
+
+# Use cases
+
+print("1! = ", factorial(1))
+print("2! = ", factorial(2))
+print("3! = ", factorial(3))
+print("4! = ", factorial(4))
+print("5! = ", factorial(5))
