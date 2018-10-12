bd2afd1c7a9eaaba00764fbaa61af5d0728dd239
Iago Breno <ibacaraujo@gmail.com>
2018-10-12 02:12:27 -0300

    Update binary_search.py

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -1,3 +1,5 @@
+# Define function
+
 def binary_search(array, target, low, high):
     if low > high:
         return False
@@ -7,6 +9,15 @@ def binary_search(array, target, low, high):
             return True
         else:
             if target < array[mid]:
-                binary_search(array, target, low, mid - 1)
+                return binary_search(array, target, low, mid - 1)
             else:
-                binary_search(array, target, mid + 1, high)
+                return binary_search(array, target, mid + 1, high)
+
+# Use case
+array = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
+target = 22
+result = binary_search(array, target, 0, len(array))
+if (result):
+    print(str(target) + " is in the array!")
+else:
+    print(str(target) + " was not found in the array.")
