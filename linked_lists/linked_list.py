5d6565b34e8aa67bafccdf3963a2ba910842dd0e
Iago Breno <ibacaraujo@gmail.com>
2018-10-13 04:11:46 -0300

    Add insert method

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -2,7 +2,22 @@ class LinkedList(Object):
     def __init__(self, head=None):
         self.head = head
 
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.set_next(self.head)
+        self.head = new_node
+
+
 class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next_node):
+        self.next_node = new_next_node
