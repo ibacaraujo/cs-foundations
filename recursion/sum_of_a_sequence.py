03ad91f71c7c2fd3403e3d64a2e26d50ce1855c8
Iago Breno <ibacaraujo@gmail.com>
2018-10-15 03:11:59 -0300

    Update sum_of_a_sequence.py

--- recursion/sum_of_a_sequence.py
+++ recursion/sum_of_a_sequence.py
@@ -1,9 +1,11 @@
+# My implementation of Summing the Elements of a Sequence Recursively
 def sum_sequence(array, n):
     # the base case
     if n == 0:
         return 0
+    # the recursive case
     else:
-        print(array[n-1])
         return array[n-1] + sum_sequence(array, n-1)
 
+# Use case
 print(sum_sequence([1,2,3,4], 4))
