2e0f1ea7d336acee661130e84da2bbc39ae8da85
Iago Breno <ibacaraujo@gmail.com>
2018-10-16 03:04:27 -0300

    Update reverse_sequence.py

--- recursion/reverse_sequence.py
+++ recursion/reverse_sequence.py
@@ -1,4 +1,10 @@
+# Define function for Reversing a Sequence with Recursion
 def reverse_sequence(array, start, stop):
     if start < stop - 1: # if at least two elements
         array[start], array[stop-1] = array[stop-1], array[start]
         reverse_sequence(array, start+1, stop-1)
+    return array
+
+# Use case
+array = [1, 2, 3, 4, 5, 6, 7, 8]
+print("Reverse ", array, "is the following: ", reverse_sequence(array, 0, len(array)))
