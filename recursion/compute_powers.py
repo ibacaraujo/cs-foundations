5b7e0b7b682a817a6b166489e999621bb97eebee
Iago Breno <ibacaraujo@gmail.com>
2018-10-16 03:16:28 -0300

    Add compute_powers.py

---
+++ recursion/compute_powers.py
@@ -0,0 +1,18 @@
+def power(x, n):
+    if n == 0:
+        return 1
+    else:
+        return x * power(x, n-1)
+
+def power2(x, n):
+    if n == 0:
+        return 1
+    else:
+        partial = power2(x, n // 2)
+        result = partial * partial
+        if n % 2 == 1:
+            result *= x
+        return result
+
+print(power(2,2))
+print(power2(2,2))
