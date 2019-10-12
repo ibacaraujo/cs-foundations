48579a9402fe2cfd574e1ec818051d403cf2d504
Iago Breno <ibacaraujo@gmail.com>
2019-10-12 14:05:29 -0300

    Create R-2.7.py

---
+++ object-oriented-programming/R-2.7.py
@@ -0,0 +1,10 @@
+# attempt
+class CreditCard:
+  def __init__(self, customer, bank, acnt, limit, balance=0):
+    self._customer = customer
+    self._bank = bank
+    self._account = acnt
+    self._limit = limit
+    self._balance = balance
+    
+ # ...
